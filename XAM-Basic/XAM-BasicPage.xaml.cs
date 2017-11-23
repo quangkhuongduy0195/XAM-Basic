761fad449d5764c657df2da9f611a2230a0decb1
DuyQK <duyqk@runsystem.net>
2017-11-23 14:42:51 +0700

    Static Resource | Resource Dictionary

--- XAM-Basic/XAM-BasicPage.xaml.cs
+++ XAM-Basic/XAM-BasicPage.xaml.cs
@@ -10,15 +10,17 @@ namespace XAMBasic
             InitializeComponent();
         }
 
-        void btnClickMe(object sender, System.EventArgs e)
+        void BtnClickMe(object sender, System.EventArgs e)
         {
             if (i % 2 == 0)
             {
-                lblName.Text = "Email: quangkhuongduy95@gmail.com";                
+                lblName.Text = "Email: quangkhuongduy95@gmail.com";
+                lblName.TextColor = Color.Red;
             }
             else 
             {
                 lblName.Text = "Quảng Khương Duy";
+                lblName.TextColor = Color.Green;
             }
             i++;
         }
