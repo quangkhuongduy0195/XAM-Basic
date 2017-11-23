1e492d71293a86d76878edc9d35440396bb98b1b
DuyQK <duyqk@runsystem.net>
2017-11-23 13:56:28 +0700

    event lick button

--- XAM-Basic/XAM-BasicPage.xaml.cs
+++ XAM-Basic/XAM-BasicPage.xaml.cs
@@ -4,9 +4,23 @@ namespace XAMBasic
 {
     public partial class XAM_BasicPage : ContentPage
     {
+        int i = 0;
         public XAM_BasicPage()
         {
             InitializeComponent();
+        }
+
+        void btnClickMe(object sender, System.EventArgs e)
+        {
+            if (i % 2 == 0)
+            {
+                lblName.Text = "Email: quangkhuongduy95@gmail.com";                
+            }
+            else 
+            {
+                lblName.Text = "Quảng Khương Duy";
+            }
+            i++;
         }
     }
 }
